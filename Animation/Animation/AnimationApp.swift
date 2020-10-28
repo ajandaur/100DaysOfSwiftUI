49f22a61fae86fa92d89f0c70aa2f093b8f6dfc8
ajandaur <anmol.jandaur@gmail.com>
2020-10-28 01:26:04 -0700

    Finished Project 5

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Anmol  Jandaur on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation/Animation/Transitions.swift
@@ -0,0 +1,50 @@
+//
+//  Transitions.swift
+//  Animation
+//
+//  Created by Anmol  Jandaur on 10/28/20.
+//
+import SwiftUI
+
+struct Transitions: View {
+    @State private var isShowingRed = false
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.scale)
+            }
+        }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+            )
+    }
+}
+struct Transitions_Previews: PreviewProvider {
+    static var previews: some View {
+        Transitions()
+    }
+}
+
