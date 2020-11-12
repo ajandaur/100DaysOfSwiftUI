7dcef8ca2cdaf155749b3a26bb7c705899c95de3
ajandaur <anmol.jandaur@gmail.com>
2020-11-12 10:56:53 -0800

    imported assets

---
+++ Milestone2/Edutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by Anmol  Jandaur on 11/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Milestone2/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,23 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Anmol  Jandaur on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct EdutainmentApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
